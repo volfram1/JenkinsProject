029e3eca1a2e27cfda5ce1b1db718ab0b2a8b98c
volfram1 <atomczak256@gmail.com>
2022-06-21 21:43:07 -0400

    2nd commit

--- src/com/groupProjects/Main.java
+++ src/com/groupProjects/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	for( int i=0; i<5; i++){
         System.out.println("hello! am i a cat?");
+        System.out.println("hello! I am a dog");
     }
     }
 }
