85d637530ae422c840c2224f8a12495c8346a7b6
volfram1 <atomczak256@gmail.com>
2022-06-21 21:09:13 -0400

    my first Jenkins project

---
+++ src/com/groupProjects/Main.java
@@ -0,0 +1,10 @@
+package com.groupProjects;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for( int i=0; i<5; i++){
+        System.out.println("hello! am i a cat?");
+    }
+    }
+}
